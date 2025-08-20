4fdc4edec92527f33309f05fc78f66b8aa2b1643
Hossein.Krouna <hossein.krouna@gmx.net>
2025-08-20 16:33:41 +0200

    feat: minimal start page (hello world)

---
+++ public/index.php
@@ -0,0 +1,13 @@
+<?php
+declare(strict_types=1);
+require __DIR__ . '/../vendor/autoload.php';
+
+?>
+<!doctype html>
+<meta charset="utf-8">
+<title>Foosball Scoreboard</title>
+
+<body style="font-family:system-ui;margin:24px">
+    <h1>Foosball Scoreboard</h1>
+    <p>App läuft!!</p>
+</body>
